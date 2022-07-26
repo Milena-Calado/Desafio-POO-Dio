35a2a795f1981e917d73ec6a10a411d6688b2580
milena.calado <mcsa2@cin.ufpe.br>
2022-07-26 17:36:19 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -24,10 +26,41 @@ public class Main {
         mentoria.setDescricao("Descricao Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Hava Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMilena = new Dev();
+        devMilena.setNome("Milena");
+        devMilena.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Milena" + devMilena.getConteudoInscritos());
+        devMilena.progredir();
+        devMilena.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Milena" + devMilena.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos Milena" + devMilena.getConteudoConcluidos());
+        System.out.println("XP:" + devMilena.calcularTotalXp());
+
+        System.out.println("--------------");
+
+        Dev devRoberta = new Dev();
+        devRoberta.setNome("Roberta");
+        devRoberta.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Roberta" + devRoberta.getConteudoInscritos());
+        devRoberta.progredir();
+        devRoberta.progredir();
+        devRoberta.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Roberta" + devRoberta.getConteudoInscritos());
+        System.out.println("Conteudos Concluidos Roberta" + devRoberta.getConteudoConcluidos());
+        System.out.println("XP:" + devRoberta.calcularTotalXp());
     }
 
 }
